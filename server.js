f3ed3c0814b5ffe5041a87f3d7e0840d51c465df
Stephan Max <stephanmax@users.noreply.github.com>
2025-10-07 03:08:59 +0000

    Remove Microsoft comments

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-'use strict';
-
 const express = require('express');
 
-// Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 app.get('/server', (req, res) => {
 	res.json({message: "Hello, from the Express server!"});
